3e9d9eb02335f40869cdd68daae4d0d1296bcb82
GiseRStewart <giserstewart@gmail.com>
2024-04-26 00:37:47 -0300

    cambios en index, cree js header,aside,footer

---
+++ static/js/aside.js
@@ -0,0 +1,26 @@
+var asideContent = `
+<aside>
+<!-- Noticias o novedades -->
+<h1>Novedades</h1>
+<div id="aside-container">
+    <h2 class="padre-h2">Las 10 mejores laptops que puedes regalar este 2024:</h2>
+    <ul id="aside-list">
+        <li><a class="hiper-aside" href="./templates/Producto.html">Apple MacBook Air M2</a></li>
+        <li><a class="hiper-aside" href="./templates/Producto.html">Dell XPS 13 (9315)</a></li>
+        <li><a class="hiper-aside" href="./templates/Producto.html">Razer Blade 14</a></li>
+        <li><a class="hiper-aside" href="./templates/Producto.html">Asus ZenBook 14 OLED</a></li>
+        <li><a class="hiper-aside" href="./templates/Producto.html">Lenovo ThinkPad Z13</a></li>
+        <li><a class="hiper-aside" href="./templates/Producto.html">Microsoft Surface Pro 9</a></li>
+        <li><a class="hiper-aside" href="./templates/Producto.html">HP Spectre x360 13.5</a></li>
+        <li><a class="hiper-aside" href="./templates/Producto.html">Dell XPS 15 (9520)</a></li>
+        <li><a class="hiper-aside" href="./templates/Producto.html">Lenovo Yoga 9i Gen 8</a></li>
+        <li><a class="hiper-aside" href="./templates/Producto.html">Lenovo Chromebook Duet 3</a></li>
+    </ul>
+    <p id="texto-aside">Cada una de ellas han sido examinada minuciosamente. Ya sea una Chromebook de buen
+        precio o una computadora portátil para juegos de primera línea, todas han sido sometidas a duras
+        pruebas en el mundo real, así como a comparativas de rendimiento y duración de la batería.
+    </p>
+</div>
+</aside>`;
+
+document.body.insertAdjacentHTML('afterbegin', asideContent)
---
+++ static/js/header.js
@@ -0,0 +1,20 @@
+var headerContent = `
+<header>
+<!-- Logo -->
+<div class="menu">
+    <img id=logo src="static/img/IdearepresentativadeLogo-removebg-preview.png" alt="">
+
+</div>
+<!-- Hipervínculos del menú -->
+<nav class="menu">
+    <ul>
+        <li><a class="hipervinculo" href="index.html">Home</a></li>
+        <li><a class="hipervinculo" href="./templates/Nosotros.html">Nosotros</a></li>
+        <li><a class="hipervinculo" href="./templates/Producto.html">Producto</a></li>
+        <li><a class="hipervinculo" href="./templates/Contacto.html">Contacto</a></li>  
+    </ul>
+</nav>
+</header>
+`;
+
+document.body.insertAdjacentHTML('afterbegin', headerContent)
