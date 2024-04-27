921c4265a274b6f9e8622ea11cdb6a6231b620fd
GiseRStewart <giserstewart@gmail.com>
2024-04-26 22:30:10 -0300

    modificaciones index, aside, footer

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,22 +1,19 @@
 var footerContent = `
 
 <footer class="pie-pagina">
+
         <div class="terminos">
             <a class="terminos-s1" href="">Términos y condiciones</a>
             <a class="terminos-s1" href="">Preguntas Frecuentes</a>
             <a class="terminos-s1" href="">Ayuda</a>
         </div>
         <div class="redes">
-            <a href="https://www.facebook.com/" target="_blank"><img class="redes-img" src="../static/img/facebook.png"
-                    alt=""></a>
-            <a href="https://www.instagram.com/" target="_blank"><img class="redes-img"
-                    src="../static/img/instagram.png" alt=""></a>
-            <a href="https://twitter.com/" target="_blank"><img class="redes-img" src="../static/img/gorjeo.png"
-                    alt=""></a>
+            <a href="https://www.facebook.com/" target="_blank"><img class="redes-img" src="../static/img/facebook.png" alt=""></a>
+            <a href="https://www.instagram.com/" target="_blank"><img class="redes-img" src="../static/img/instagram.png" alt=""></a>
+            <a href="https://twitter.com/" target="_blank"><img class="redes-img" src="../static/img/gorjeo.png" alt=""></a>
         </div>
-        <div class="copy"> © 2020 <code translate="no"></code>
-        </div>
-        </footer>
+        <div class="copy"> © 2020 <code translate="no"></code></div>
+</footer>
 
 `;
 document.body.insertAdjacentHTML('afterbegin', footerContent)
