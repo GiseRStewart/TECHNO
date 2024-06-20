792732e52f3e1066f035ff969f721c288044d983
GiseRStewart <giserstewart@gmail.com>
2024-06-20 19:16:59 -0300

    agregue registro

--- Backend/techno_db/DB/querys.sql
+++ Backend/techno_db/DB/querys.sql
@@ -7,9 +7,25 @@ CREATE TABLE notebook (
     modelo VARCHAR(100),
     precio INT,
     cantidad INT, 
-    descripcion VARCHAR(100)
+    descripcion TEXT
 );
+DROP TABLE notebook;
+
 
 SHOW TABLES;
 SELECT * FROM productos_db.notebook;
+
+    INSERT INTO notebook (
+    modelo,
+    precio,
+    cantidad,
+    descripcion) 
+    VALUES (
+    'Apple MacBook Air M2',
+    2719900,
+    10,
+    'CPU: Apple M2 Chip RAM: 8GB GPU: Apple M2 8-Core Pantalla: 13.3-pulgadas Retina Almacenamiento: 256GB SSD Dimensiones: 11.97 x 8.36 x 0.63 pulgadas'
+    );
+
+
 
