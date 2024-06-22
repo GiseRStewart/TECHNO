19ca83e840ebaa9216e41817bc979c5a65dc729b
GiseRStewart <giserstewart@gmail.com>
2024-06-21 22:19:16 -0300

    modificaciones

--- Frontend/static/js/header.js
+++ Frontend/static/js/header.js
@@ -2,7 +2,7 @@ var headerContent = `
 <header>
     <!-- Logo -->
     <div class="menu">
-        <img id="logo" src="../static/img/IdearepresentativadeLogo-removebg-preview.png" alt="">
+    <img id="logo" src="./static/img/IdearepresentativadeLogo-removebg-preview.png" alt="">
 
     </div>
     <!-- Hipervínculos del menú -->
@@ -12,10 +12,11 @@ var headerContent = `
         <i class="fa-solid fa-bars"></i>
         </label>
         <ul>
-            <li><a class="hipervinculo" href="../index.html">Home</a></li>
-            <li><a class="hipervinculo" href="../templates/Nosotros.html">Nosotros</a></li>
-            <li><a class="hipervinculo" href="../templates/Producto.html">Producto</a></li>
-            <li><a class="hipervinculo" href="../templates/Contacto.html">Contacto</a></li>
+            <li><a class="hipervinculo" href="index.html">Home</a></li>
+            <li><a class="hipervinculo" href="./templates/Nosotros.html">Nosotros</a></li>
+            <li><a class="hipervinculo" href="./templates/Producto.html">Producto</a></li>
+            <li><a class="hipervinculo" href="./templates/Contacto.html">Contacto</a></li>
+            <li><a class="hipervinculo" href="./tabla_productos.html">Tabla</a></li>
         </ul>
     </nav>
 </header>
